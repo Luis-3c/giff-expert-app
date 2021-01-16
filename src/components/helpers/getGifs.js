737a3eeb80eef68d2efef860819225bb3f433582
Luis <gerard95@live.com.mx>
2021-01-16 17:51:33 -0600

    first commit

---
+++ src/components/helpers/getGifs.js
@@ -0,0 +1,14 @@
+export const getGifs = async(category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=Zg7P7B0e5RNCbXVNuvCf3J0fRw8E9zuY`;
+    const res = await fetch(url);
+    const {data} = await res.json();
+
+    const gifs = data.map( img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    })
+    return gifs;
+}
